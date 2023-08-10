100aeb16697948a4a6e97f879429b0e471d8ecca
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-10 14:57:14 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main-an entry point function in C
+ *puts-outputs string specified
+ *
+ *Return: ends function with 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	puts("");
+	return (0);
+}
