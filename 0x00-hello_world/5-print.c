c6b7b7b0f0aa511419fb490ac3bebfab85bc9342
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-10 15:32:56 -0700

    Print program that prints line

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *printf-outputs formatted output
+ *
+ *Return: 0 to show (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
