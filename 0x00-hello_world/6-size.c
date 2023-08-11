47f0808e27f69498ae8dcdd5ed6c1f7bcd6be398
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-10 17:43:17 -0700

    Fic typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- *Main - Entry point
- *Printf()-outputs size of types
+ *main - Entry point
+ *Printf-outputs size of types
  *
  *Return: 0 (SUCCESS)
  */
 
 int main(void)
 {
-	prinf("Size of a char: %zu byte(s)\n", sizeof(char));
-	prinf("Size of a int: %zu byte(s)\n", sizeof(int));
-	prinf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	prinf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	prinf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
