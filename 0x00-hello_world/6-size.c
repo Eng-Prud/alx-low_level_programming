0c1b5b28426c8d0811f1ac14c5426bd5e8b5e3ca
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-10 17:39:28 -0700

    Print types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 /**
- *main-Entry level
- *printf-output sizes of various types in computer
+ *Main - Entry point
+ *Printf()-outputs size of types
  *
- *Return: Displays 0 (SUCCESS)
+ *Return: 0 (SUCCESS)
  */
+
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	prinf("Size of a char: %zu byte(s)\n", sizeof(char));
+	prinf("Size of a int: %zu byte(s)\n", sizeof(int));
+	prinf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	prinf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	prinf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
