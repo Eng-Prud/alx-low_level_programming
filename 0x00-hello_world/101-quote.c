f96804e993930daf76c6b076e19924b067f48193
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-10 17:09:53 -0700

    Print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-Entry Level
+ *write(2,-outputs to the standard error
+ *
+ *Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
