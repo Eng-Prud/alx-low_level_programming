3210708063da5e2f08e2044c5fb3b186014f089f
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-11 12:03:42 -0700

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *putchar - prints to standard output
+ *
+ *Return: always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter < 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
