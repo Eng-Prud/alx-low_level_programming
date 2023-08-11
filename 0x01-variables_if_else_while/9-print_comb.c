c7baacfd579086d65799982cb4a2db45905967fa
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-11 14:17:58 -0700

    print allpossible number combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry level
+ *putchar - prints all possible cominations of single digit numbers
+ *
+ *Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
