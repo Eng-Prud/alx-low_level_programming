eba7e2a8e7563a4aa8cf0726719c1c6aa4b01cd3
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-15 18:33:46 -0700

    Add space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,20 +20,29 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
+
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
 			}
+
 			if (product < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');
-				_putchar((product % 10) + '0');
 			}
 			else if (product >= 10 && product < 100)
 			{
 				_putchar(' ');
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+			}
+
+			_putchar((product % 10) + '0');
+
+			if (column == 9)
+			{
+				_putchar('\n');
 			}
 		}
-		_putchar('\n');
 	}
 }
