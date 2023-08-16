6807f8f6e280cfe0f0dcaa538b1d37fafb705a4c
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-16 14:56:25 -0700

    Fibonacci sequence whose values do not exceed 4,000,000,

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Level
+ * Printf - Displays the sum of even valued terms
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long previous = 1;
+	long current = 2;
+	long next;
+	long sum = current;
+
+	while (current + previous < 4000000)
+	{
+		current += previous;
+		if (current % 2 == 0)
+		{
+			sum += current;
+			previous = current - previous;
+			++i;
+		}
+		printf("%ld\n", sum);
+		return (0);
+	}
+}
