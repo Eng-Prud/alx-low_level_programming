205855c6189bcd685a7e61c6f6be60dd77ca9a69
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-17 09:08:52 -0700

    Add comments

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+/**
+ * _isupper - function to print uppercase letters
+ * @c: entered letter
+ *
+ * Return: 1 if uppercase 0 if lowercase
+ */
+
 int _isupper(int c)
 {
-        if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
