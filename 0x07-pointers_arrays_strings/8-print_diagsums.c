09a6db9608cfa59bfe30e1905d49fd7f5db47e7c
Prudence Njoroge <prudence.n826@gmail.com>
2023-08-29 03:39:17 -0700

    Chmod the file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,45 +12,16 @@ void print_diagsums(int *a, int size)
 	int sum_diag1 = 0;
 	int sum_diag2 = 0;
 	int i;
-	int temp = sum_diag1
-	int digits = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum_diag1 += a[i * size + 1];
+		sum_diag1 = sum_diag1 + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
 		sum_diag2 += a[i * size + (size - i - 1)];
 	}
 
-	while (temp != 0)
-	{
-		temp /= 10;
-		digits++
-	}
-
-	while (digits > 0)
-	{
-		int digit - sum_diag1 / pow(10, digits - 1);
-		_putchar(digit + '0');
-		sum_diag1 %= (int)pow(10, digits - 1);
-		digits--;
-	}
-	_putchar(',');
-
-	temp = sum_diag2;
-	digits = 0;
-
-	while (temp !+0)
-	{
-		temp /= 10;
-		digits++;
-	}
-	while (digits > 0)
-	{
-		int digit = sum_diag2 / pow(10l digits - 1);
-
-		_putchar(digit + '0');
-		sum_diag2 %= (int)pow(10, digits - 1);
-		digits--;
-	}
-	_putchar('\n');
+	_putchar(sum_diag1 + '0');
+	_putchar(sum_diag2 + '0');
 }
