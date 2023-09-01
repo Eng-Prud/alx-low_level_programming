b5ad366a2f7f491bcc2d48b4214fa688bf8bcd35
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-01 14:27:10 -0700

    Modify my return values

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,18 +5,19 @@
  * main - program that multiplies two numbers
  * @argc: counts arg
  * @argv: Array of pointers to strings
- * Return: 1
+ * Return: 1 on error and 0 on SUCCESS
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc > 2)
+	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
 }
