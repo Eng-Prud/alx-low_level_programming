b9a68ce71542adf24fb4dcfb9ab839700e72cdee
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-13 10:32:55 -0700

    Add error handling

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t p;
 
+	if (array == NULL || action == NULL)
+		return;
 	for (p = 0; p < size; p++)
 	{
 		action(array[p]);
