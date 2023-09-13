9e8199ea2d8a65df58b00c4bac9fa7d1d1f11317
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-13 14:01:50 -0700

    Check betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,16 +3,20 @@
 #include <stddef.h>
 
 
- op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
 
+/**
+ * get_op_func - Function that selects correct operation
+ * @s: opperator
+ * @Return: Return ptr to function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -32,13 +32,13 @@ int main(int argc, char *argv[])
 	if (operation == NULL)
 	{
 		printf("Error\n");
-		return 99;
+		return (99);
 	}
 
 	if ((*operator == '/' || *operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		return 100;
+		return (100);
 	}
 	calc = operation(num1, num2);
 	printf("%d\n", calc);
