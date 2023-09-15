a6eb8ee691a318c3ff9cf7f2e4571752dc1d2ddd
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-14 20:13:44 -0700

    add bracket on nil

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,6 @@ void print_all(const char * const format, ...)
 			case 'c':
 				printf("%s%c", separator, va_arg(arg, int));
 				break;
-
 			case 'i':
 				printf("%s%d", separator, va_arg(arg, int));
 				break;
@@ -34,7 +33,7 @@ void print_all(const char * const format, ...)
 				{
 					string = va_arg(arg, char *);
 					if (string == NULL)
-						string = "(nil";
+						string = "(nil)";
 					printf("%s%s", separator, string);
 				}
 				break;
