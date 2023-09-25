8087fb7cbe970f1d210b66aaca3f7eb2672fdbc9
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-25 11:04:12 -0700

    Update files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,10 +21,8 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
-
 		h = h->next;
 		num_nodes++;
 	}
-
 	return (num_nodes);
 }
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+More tasks on Singly Linked Lists
