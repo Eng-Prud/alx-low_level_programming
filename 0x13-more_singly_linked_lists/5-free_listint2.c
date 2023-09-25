023ee7f8c3fff77f8e4ca9f438201772db03852e
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-25 13:39:58 -0700

    Add ptr signs

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *loop;
 
-	if (head != NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return;
 	}
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		loop = *head;
 		*head = loop->next;
 		free(loop);
 	}
-	*head = NULL;
 }
