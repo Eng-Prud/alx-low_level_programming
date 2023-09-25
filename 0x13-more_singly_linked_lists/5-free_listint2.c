98db44033123ef8509cf7db0bc5b8342876085ff
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-25 13:31:37 -0700

    Freelist and set hed to Null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: ptr to head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *loop;
+
+	if (head != NULL)
+	{
+		return;
+	}
+
+	while (head != NULL)
+	{
+		loop = *head;
+		*head = loop->next;
+		free(loop);
+	}
+	*head = NULL;
+}
