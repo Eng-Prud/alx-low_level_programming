1734eab8e0e234e0df75f03501f8ae30360b2f5c
Prudence Njoroge <prudence.n826@gmail.com>
2023-09-26 11:20:08 -0700

    Add all data in all  nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data (n) of a list
+ * @head: ptr to head of the list
+ * Return: sum of alldata(n), 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
