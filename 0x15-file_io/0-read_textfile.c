551f032458c8bc1047c3f3a1fea4ae7b2f91919b
Eng-Prud <prudence.n826@gmail.com>
2023-10-30 20:49:06 +0300

    Codes for File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: string of chars
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (NULL);
+	}
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	ssize_t bytesRead = fread(buffer, 1, letters, file);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1)
+	{
+		free(buffer);
+		fclode(file);
+		return (0);
+	}
+
+	free(buffer);
+	fclose(file);
+
+	return (bytesWritten);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on SUCCESS, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		ssize_t write_result = write(fd, text_content,
+				strlen(text_content));
+		if (write_result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	clode(fd);
+	return (1);
+}
