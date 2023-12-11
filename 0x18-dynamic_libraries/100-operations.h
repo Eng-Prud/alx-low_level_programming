f5bfb5781c3e4d54affd57947a11046ac8370d5b
Eng-Prud <Prudence.n826@gmai.com>
2023-12-11 02:23:42 -0800

    call c functions in dynamic library from python

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,13 @@
+// 100-operations.h
+
+#ifndef _100_OPERATIONS_H
+#define _100_OPERATIONS_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* _100_OPERATIONS_H */
+
