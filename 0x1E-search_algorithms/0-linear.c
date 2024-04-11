bbf5982c2dae25eb987d2632c04c679160bfc52a
Eng-Prud <Prudence.n826@gmai.com>
2024-04-11 09:33:38 -0700

    fix betty issue on task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "search_algos.h"
+
 /**
- * linear_search - searches for a value in an array of
+ * linear_search - searches for a value in an array of integers
+ *
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
  * Return: the index of the found value,
- * or -1 if not found
+ *         or -1 if the value is not found
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
